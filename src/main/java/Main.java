d0e4ad1c5d797f9d7682c156f9daf41c09e75ea8
Justincjr <jchoojr@hotmail.com>
2024-08-28 17:50:22 +0800

    Add third iteration with comments handleUserInput updated to use our new ways of creating DialogBox instances.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -105,9 +105,8 @@ public class Main extends Application {
         String userText = userInput.getText();
         String dukeText = duke.getResponse(userInput.getText());
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, userImage),
-                //add in duke response
-                new DialogBox(dukeText, dukeImage)
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage)
         );
         userInput.clear();
     }
