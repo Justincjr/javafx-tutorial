de88e5a4418b278e1b62422bdef974c68be8f3ae
Justincjr <jchoojr@hotmail.com>
2024-08-28 17:08:14 +0800

    Add DialogBox

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+public class Main extends Application {
+    private static final String DEFAULT_FILE_PATH = "duke/example.txt";
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    public Main(String filePath) {
+        // ...
+    }
+
+    // Overloaded constructor
+    public Main() {
+        this(DEFAULT_FILE_PATH);
+    }
+    @Override
+    public void start(Stage stage) {
+        //Setting up required components
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //More code to be added here later
+    }
+}
